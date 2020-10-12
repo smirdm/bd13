4c024c67c5ed09f206c6ae1452716346b9cd1be0
smirdm <62605515+smirdm@users.noreply.github.com>
2020-10-13 01:34:02 +0300

    Update lab1s_m.py

--- lab1s_m.py
+++ lab1s_m.py
@@ -4,6 +4,5 @@ import sys
 
 for line in sys.stdin:
 	
-	u1, u2, country, u3, payment = line.strip().split(',')
-	print '\t'.join([country.strip(), payment.strip()])
-
+	u1, u2, country, u3, payout = line.strip().split(',')
+	print '\t'.join([country.strip(), payout.strip()])
